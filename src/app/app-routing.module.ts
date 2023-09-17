41b6621deab62c7d02f075734186803e88731008
Vasily Pavlov <strix.wire@gmail.com>
2023-09-17 18:56:01 +0700

    base navbar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+
+const routes: Routes = [
+    {
+        path: '', component: MainLayoutComponent, children: [
+            {path: '', redirectTo: '/', pathMatch:'full'},
+            {path: '', component: HomePageComponent}
+        ]
+    }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
